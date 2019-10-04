e32067c4ac62ee9216e1d616e7653d07209db67b
thistonyb <this.tonybaker@gmail.com>
2019-10-04 11:37:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  let data = {
+    name: userName,
+    email: userEmail
+  };
+
+  let configObj = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(object => {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+      console.log(error.message);
+    });
+}
