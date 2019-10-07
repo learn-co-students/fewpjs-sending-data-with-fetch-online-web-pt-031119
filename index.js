3f72c2f2972ca3f0a39ebf18198b7f8b70972c68
“Mark <mkanko66@gmail.com>
2019-10-06 20:05:27 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Add your code here
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            const body = document.querySelector('body');
+            const p = document.createElement('p');
+            p.innerHTML = object['id'];
+            body.appendChild(p);
+        })
+        .catch(function(error) {
+            alert('Bad Monkey!');
+            const body = document.querySelector('body');
+            const p = document.createElement('p');
+            p.innerHTML = error.message;
+            body.appendChild(p);           
+        })
+    
+};
+
+
+
+
+
+
+
+// let formData = {
+//     dogName: 'Byron',
+//     dogBreed: 'Poodle'
+// };
+
+// let configObj = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch('http://localhost:3000/dogs', configObj)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error){
+//         alert('Bad things!');
+//     });
+
+
+// fetch('http://localhost:3000/dogs', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         dogName: 'Byron',
+//         dogBreed: 'Poodle'
+//     })
+// });
