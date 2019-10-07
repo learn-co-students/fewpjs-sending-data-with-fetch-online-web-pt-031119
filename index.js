975045c37a350d039322de38e5d83380e1b6c6b3
Michael Schiumo <mschiumo18@gmail.com>
2019-10-07 18:44:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+// let errorMessage = {
+//   message: "Unauthorized Access"
+// };
+
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+    document.body.innerHTML = object.id;
+  })
+  .catch(function(error){
+    alert("Something went wrong!");
+    console.log(error.message);
+    document.body.innerHTML = error.message;
+  });
+};
