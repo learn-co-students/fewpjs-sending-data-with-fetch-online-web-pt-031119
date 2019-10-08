0becd901b27c906fe9f98b4e6e85a426edb0797a
Ryan <kerneltux0@gmail.com>
2019-10-08 09:43:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+const destURL = 'http://localhost:3000/users';
+const page = document.body;
+
+// document.addEventListener('DOMContentLoaded', function(){
+//   submitData(userName, userEmail);
+// });
+
+function submitData(userName, userEmail){
+  const formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  const configObject={
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch(destURL, configObject)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(object){
+    // for(const element of object){
+    // console.log(object.id)
+    page.innerHTML = `${object.id}`;
+    // };
+    // return fetch(object);
+  })
+  .catch(function(error){
+    alert('Unauthorized Access');
+    page.innerHTML = 'Unauthorized Access';
+  });
+
+};
