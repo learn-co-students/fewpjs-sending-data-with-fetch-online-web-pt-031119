79aaa1f9a42af8bde90980658d4d339fb7bb11b9
Alex Merecka <merecka@gmail.com>
2019-10-14 13:53:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+let formData = {
+  name: `${name}`,
+  email: `${email}`
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+
+
+function submitData(name, email) {
+	return fetch("http://localhost:3000/users", configObj)
+	  .then(function(response) {
+	    return response.json();
+	  })
+	  .then(function(object) {
+	    console.log(object);
+	  })
+	  .catch(function(error) {
+	    alert("Bad things! Ragnarők!");
+	    console.log(error.message);
+	  });
+  }
