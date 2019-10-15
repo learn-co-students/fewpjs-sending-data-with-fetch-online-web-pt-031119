1dfac58efa54374970918863f3cc17d6c17b1cac
Alex Merecka <merecka@gmail.com>
2019-10-14 19:00:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-let formData = {
-  name: `${name}`,
-  email: `${email}`
-};
- 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-
-
 function submitData(name, email) {
+	let formData = {
+  	name: `${name}`,
+ 	email: `${email}`
+		};
+
+	let configObj = {
+	  method: "POST",
+	  headers: {
+	    "Content-Type": "application/json",
+	    "Accept": "application/json"
+	  },
+	  body: JSON.stringify(formData)
+	};
+
 	return fetch("http://localhost:3000/users", configObj)
 	  .then(function(response) {
 	    return response.json();
 	  })
 	  .then(function(object) {
-	    console.log(object);
+	  	let element = document.createElement('p')
+	  	document.body.appendChild(element)
+	  	element.innerHTML = object.id
+	  	
 	  })
 	  .catch(function(error) {
 	    alert("Bad things! Ragnarők!");
-	    console.log(error.message);
+	    let error_msg = document.createElement('p')
+	    document.body.appendChild(error_msg)
+	    error_msg.innerHTML = error
 	  });
   }
